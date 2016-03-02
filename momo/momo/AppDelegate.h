ae283b7b2471ef0d4a21e1428f3e31b6dd9e7174
canhua <canhua@canhuademacbook-pro.local>
2016-03-02 19:09:00 +0800

    初始化momo

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by canhua on 16/3/2.
+//  Copyright © 2016年 canhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo
+//
+//  Created by canhua on 16/3/2.
+//  Copyright © 2016年 canhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
