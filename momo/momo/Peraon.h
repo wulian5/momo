d2e93a251d3c57c90f550b1d421211316302dbbb
canhua <canhua@canhuademacbook-pro.local>
2016-03-02 19:11:13 +0800

    添加Person类

---
+++ momo/momo/Peraon.h
@@ -0,0 +1,15 @@
+//
+//  Peraon.h
+//  momo
+//
+//  Created by canhua on 16/3/2.
+//  Copyright © 2016年 canhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Peraon : UIViewController
+
+/**name*/
+@property (nonatomic,strong) NSString *name;
+@end
---
+++ momo/momo/Peraon.m
@@ -0,0 +1,13 @@
+//
+//  Peraon.m
+//  momo
+//
+//  Created by canhua on 16/3/2.
+//  Copyright © 2016年 canhua. All rights reserved.
+//
+
+#import "Peraon.h"
+
+@implementation Peraon
+
+@end
